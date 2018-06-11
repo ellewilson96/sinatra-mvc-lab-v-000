abf0f0fc0259ccb3e47dfd2055d9af6d31135ce3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-11 22:08:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,11 +2,24 @@ class PigLatinizer
   attr_reader :user_phrase
 
   def initialize(user_phrase)
-    @user_phrase = user_phrase.split(" ")
+    @user_phrase = user_phrase
   end
 
-  def 
+  def
 
-  def piglatinize
+  def piglatinize str
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
+
+  if vowels.include?(str[0])
+    str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0])
+    str[1..-1] + str[0] + 'ay'
+  else
+    str # return unchanged
+  end
   end
 end
